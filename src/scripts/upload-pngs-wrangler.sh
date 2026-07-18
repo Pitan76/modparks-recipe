5a0496bdaf7fd1653cd8999ae5b5084b13941aca
PTOM76 <admin@pitan76.net>
2026-07-18 23:40:22 +0900

    feat: add scripts for isometric SVG generation, texture processing, and R2 asset deployment

---
+++ src/scripts/upload-pngs-wrangler.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/user/workspace/modparks-recipe/render_out_png
+
+# List all files and run wrangler r2 object put in parallel
+find . -name "*.png" | sed 's/^\.\///' | xargs -I {} -P 5 npx wrangler r2 object put "mp-recipe-images/assets/minecraft/textures/render/{}" --file "{}" --content-type "image/png"
