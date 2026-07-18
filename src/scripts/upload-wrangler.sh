1867f8a5f133d4f906d6850c9faf68cb63048cc6
PTOM76 <admin@pitan76.net>
2026-07-19 00:12:19 +0900

    Fix rendering issues and remove hardcoded paths

--- src/scripts/upload-wrangler.sh
+++ src/scripts/upload-wrangler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/user/workspace/modparks-recipe/render_out
+cd "$(dirname "$0")/../../render_out"
 
 # List all files and run wrangler r2 object put in parallel
 find . -name "*.svg" | sed 's/^\.\///' | xargs -I {} -P 5 npx wrangler r2 object put "mp-recipe-images/assets/minecraft/textures/render/{}" --file "{}" --content-type "image/svg+xml"
